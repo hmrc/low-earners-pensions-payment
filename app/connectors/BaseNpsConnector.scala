48d952eb2f7bfdb715b033627d503f8600979763
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2026-09-03 12:35:20 +0100

    Removed backend response body error log (#17)

--- app/connectors/BaseNpsConnector.scala
+++ app/connectors/BaseNpsConnector.scala
@@ -92,7 +92,6 @@ abstract class BaseNpsConnector[Resp: Reads] extends HttpErrorFunctions { this: 
           Right[ErrorWrapper, ResponseWrapper[Rds]](ResponseWrapper(correlationId, value)).withLeft
         case JsError(errors) =>
           logger.error(s"$extraContext - $methodLoggingContext: Json validation failed")
-          logger.error(s"[TEMPORARY-ERROR-PLEASE-REMOVE] - $responseJson")
           Left(ErrorWrapper(correlationId, InternalLeppError))
       }
     } catch {
